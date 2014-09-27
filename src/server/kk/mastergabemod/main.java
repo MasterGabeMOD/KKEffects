794f72e77c974c9c16f03adc1ebcef6e7c833843
MasterGabeMOD <MasterGabeMod@yahoo.com>
2014-09-27 14:56:31 -0700

    Just some fixes and cleanups

--- src/server/kk/mastergabemod/main.java
+++ src/server/kk/mastergabemod/main.java
@@ -1,6 +1,8 @@
 package server.kk.mastergabemod;
 
+import java.io.File;
 import java.util.Iterator;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,16 +19,29 @@ public class main extends JavaPlugin
     {
     }
 
-    public void onEnable() {
-    	getServer().getPluginManager().registerEvents(this, this);
-        getConfig().options().copyDefaults(true);
-        saveConfig();
-    }
-    
+    @Override
+    public void onEnable()
+    {
+        File file = new File(getDataFolder() + File.separator + "config.yml");
 
+        if(!file.exists())
+        {
+            this.getLogger().info("Generating config.yml...");
+            this.getLogger().info("Default configuration generated successfully!");
+        }
+
+
+        this.getLogger().info("[KKEffects] has been enabled!");
+
+    }
+
+    @Override
     public void onDisable()
     {
+
+        this.getLogger().info("[KKEffects] has been disabled!");
     }
+
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event)
@@ -35,7 +50,7 @@ public class main extends JavaPlugin
         if(getConfig().getBoolean("RemovePotions") && player.hasPermission("kkeffects.remove"))
         {
             PotionEffect effect;
-            for(Iterator iterator = player.getActivePotionEffects().iterator(); iterator.hasNext(); player.removePotionEffect(effect.getType()))
+            for(Iterator<PotionEffect> iterator = player.getActivePotionEffects().iterator(); iterator.hasNext(); player.removePotionEffect(effect.getType()))
                 effect = (PotionEffect)iterator.next();
 
         }
