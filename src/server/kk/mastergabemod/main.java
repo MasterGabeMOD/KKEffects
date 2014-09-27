255f58cb1bc358dbda4fd73b47fd46b1e271b85b
MasterGabeMOD <MasterGabeMod@yahoo.com>
2014-09-27 14:45:33 -0700

    Import Files

---
+++ src/server/kk/mastergabemod/main.java
@@ -0,0 +1,51 @@
+package server.kk.mastergabemod;
+
+import java.util.Iterator;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class main extends JavaPlugin
+    implements Listener
+{
+
+    public main()
+    {
+    }
+
+    public void onEnable() {
+    	getServer().getPluginManager().registerEvents(this, this);
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+    
+
+    public void onDisable()
+    {
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event)
+    {
+        Player player = event.getPlayer();
+        if(getConfig().getBoolean("RemovePotions") && player.hasPermission("kkeffects.remove"))
+        {
+            PotionEffect effect;
+            for(Iterator iterator = player.getActivePotionEffects().iterator(); iterator.hasNext(); player.removePotionEffect(effect.getType()))
+                effect = (PotionEffect)iterator.next();
+
+        }
+        
+        if(getConfig().getBoolean("JumpEnabled") && player.hasPermission("kkjump.jump"))
+            player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, getConfig().getInt("JumpTime"), getConfig().getInt("JumpHeight")));
+        if(getConfig().getBoolean("SpeedEnabled") && player.hasPermission("kkeffects.speed"))
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, getConfig().getInt("SpeedTime"), getConfig().getInt("SpeedLevel")));
+        if(getConfig().getBoolean("NightVisionEnabled") && player.hasPermission("kkeffects.nightvision"))
+            player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, getConfig().getInt("NightVisionTime"), getConfig().getInt("NightVisionLevel")));
+    }
+}
+
